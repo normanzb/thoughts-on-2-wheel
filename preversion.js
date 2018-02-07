155f34bf1aa05aadaaf1b49b55d77ad98b71fbad
Norman Xu <im@norm.im>
2018-02-07 16:35:40 +0000

    fix file name

--- preversion.js
+++ preversion.js
@@ -1,6 +1,6 @@
 const pkg = require('./package.json');
 const fs = require('fs');
-const FILENAME = 'README.md';
+const FILENAME = 'partials/styles.html';
 
 var readmeContent = fs.readFileSync(FILENAME, 'utf8');
 readmeContent = readmeContent.replace(new RegExp(pkg.version, 'g'), '{placeholder}');
